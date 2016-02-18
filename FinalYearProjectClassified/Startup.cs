9cc6a52880e49755ec40f80c012ecdb2be29f292
lmacha02 <luis_machado_pnf@hotmail.com>
2016-02-18 14:23:04 +0000

    Test change v01

---
+++ FinalYearProjectClassified/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinalYearProjectClassified.Startup))]
+namespace FinalYearProjectClassified
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
